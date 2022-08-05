acfbec83548d40ad62758266ee22b0c50f888c2f
ForrestLyons <forrestjlyons@gmail.com>
2022-08-04 22:07:47 -0600

    exercise 3 and two

--- starter/problems/03-set-key-in-object.js
+++ starter/problems/03-set-key-in-object.js
@@ -1,27 +1,33 @@
 /***********************************************************************
-Write a function `setKeyInObject(obj, string, value)` that takes in three
-parameters. The first parameter is an object, the second parameter will be a
+Write a function `setKeyInObject(obj, string, value)` that takes in three 
+parameters. The first parameter is an object, the second parameter will be a 
 string and the third parameter will be a value. Your job is to return the object
 adding the second parameter as a key using the third parameter as its value.
 
 Examples:
 
-let obj = {}
-setKeyInObject(obj, "apple", "yum"); // => {apple: "yum"}
-
-let obj1 = {str: "hello"}
-setKeyInObject(obj1, "num", 3); // => {str: "hello", num: 3}
-
-Figure out the method of defining a key and value
-push that into the object
-return obj
 
 ***********************************************************************/
-
+//declare a value variable thats equal to string
 function setKeyInObject(obj, string, value) {
-  // your code here
-  
+  obj[string] = value;
+  return obj;
 }
 
+// function setKeyInObject(obj, string, value) {
+//   let val = string;
+//   obj[val] = value;
+//   return obj;
+ 
+//  }
+
+
+
+
+let obj = {}
+console.log(setKeyInObject(obj, "apple", "yum")); // => {apple: "yum"}
+
+let obj1 = {str: "hello"}
+console.log(setKeyInObject(obj1, "num", 3)); // => {str: "hello", num: 3}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = setKeyInObject;
